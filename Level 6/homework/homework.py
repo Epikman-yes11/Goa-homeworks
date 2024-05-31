5d4af5574c7c1fc4cd8353030c3bfc17b172c20d
Epikman-yes11 <davidberaia11@gmail.com>
2024-05-31 21:22:51 +0400

    commit

---
+++ Level 6/homework/homework.py
@@ -0,0 +1,10 @@
+name = "data" #name is data
+surname = "diasamidze" #surname is diasamidze
+age = 100 # age is 100
+text = "I am programmer" #he is saying that he is a programmer
+#now lets make him say what his name is what his surname is what his age is and what is his proffesion
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
+
+
+
+
